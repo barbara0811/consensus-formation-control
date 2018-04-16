85df958197fd36222e67fe63948c339e60974da0
Marijana <35827103+marijana23@users.noreply.github.com>
2018-04-16 18:11:21 +0200

    Add files via upload
    pokrenuti Initial.m i onda simulaciju

--- Visual.m
+++ Visual.m
@@ -9,7 +9,7 @@ ymin = -100;
 agents = [];
 trajectories = [];
 nametag = [];
-colors = {[0.6,0.9,0.6],[0,0.7,1],[0.85, 0.6,0.85],[1,0.8,0],[0.9,0.6,0.5]};
+colors = {[0.6,0.9,0.6],[0,0.7,1],[0.85, 0.6,0.85],[1,0.8,0],[0.9,0.6,0.5],[0,0,1]};
 for i = 1:x.signals.dimensions
     agents = cat(2,agents, animatedline('MaximumNumPoints',1,'Marker', 'o','Color', cell2mat(colors(i))));
     trajectories = cat(2,trajectories,animatedline('Color', cell2mat(colors(i))));
@@ -63,6 +63,72 @@ for i= 1:9
         end
     end   
 end
+
+%crtanje bitnih podataka
+f3 = figure('Name','3');
+subplot(2,2,1)
+for i = 1:x.signals.dimensions
+    plot(x.time(:), x.signals.values(:,i),'Color', cell2mat(colors(i)));
+    hold on;
+end
+ylabel('x')
+
+subplot(2,2,2)
+for i = 1:y.signals.dimensions
+    plot(y.time(:), y.signals.values(:,i), 'Color', cell2mat(colors(i)));
+    hold on;
+end
+ylabel('y')
+xlabel('t')
+
+% subplot(2,2,3)
+% for i = 1:x_vel.signals.dimensions
+%     plot(x_vel.time(:), x_vel.signals.values(:,i),'Color', cell2mat(colors(i)));
+%     hold on;
+% end
+% ylabel('x_velocity')
+% legend(Nametag);
+% 
+% subplot(2,4,4)
+% for i = 1:y_vel.signals.dimensions
+%     plot(y_vel.time(:), y_vel.signals.values(:,i),'Color', cell2mat(colors(i)));
+%     hold on;
+% end 
+% ylabel('x_velocity')
+% xlabel('t')
+
+% 
+% f4 = figure('Name','4');
+% 
+% vel = sqrt(y_vel.signals.values.^2 + x_vel.signals.values.^2);
+% 
+% subplot(2,1,1);
+% for i = 1:vel.signals.dimensions
+%     plot(y_vel.time(:), y_vel.signals.values(:,i),'Color', cell2mat(colors(i)));
+%     hold on;
+% end 
+% ylabel('velocity')
+% legend(Nametag);
+
+% subplot(2,1,2);
+% for i = 1:f_c.signals.dimensions
+%     plot(f_c.time(:), f_c.signals.values(:,i),'Color', cell2mat(colors(i)));
+%     hold on;
+% end 
+% xlabel('t')
+% ylabel('centar')
+
+%crtanje trajektorija bez animacije
+% f5 = figure('Name','5');
+% xlabel('x')
+% ylabel('y')
+% axis([xmin,xmax,ymin,ymax]);
+% legend(Nametag);
+
+% for i = 1:vel.signals.dimensions
+%     plot(x.signals.values(:,i), y.signals.values(:,i),'Color', cell2mat(colors(i)));
+%     hold on;
+% end 
 
 
 
